c2d040b3de5041b18d104433e85cac6bd09985f7
NguyenHao <nguyenhao.mailfortest@gmail.com>
2020-12-09 22:45:12 +0700

    update db

--- src/main/java/com/bachkhoa/database/db.sql
+++ src/main/java/com/bachkhoa/database/db.sql
@@ -1,23 +1,36 @@
 use app;
-insert into role(code,name) values('ADMIN','Quản trị');
-insert into role(code,name) values('USER','Người dùng');
+
+insert into role(code,name) values('ADMIN','Administrator');
+insert into role(code,name) values('USER','Employee');
 
 insert into user(username,password,fullname,status)
 values('admin','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG','Administrator',1);
 insert into user(username,password,fullname,status)
-values('user1','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG','nguyễn văn A',1);
+values('user1','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG','Employee01',1);
 insert into user(username,password,fullname,status)
-values('user2','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG','nguyễn văn B',1);
+values('user2','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG','Employee02',1);
 
 INSERT INTO user_role(userid,roleid) VALUES (1,1);
 INSERT INTO user_role(userid,roleid) VALUES (2,2);
 INSERT INTO user_role(userid,roleid) VALUES (3,2);
 
-INSERT INTO `app`.`category` (`code`, `name`) VALUES ('The-thao', 'Thể thao');
-INSERT INTO `app`.`category` (`code`, `name`) VALUES ('Van-hoc', 'Văn học');
-INSERT INTO `app`.`category` (`code`, `name`) VALUES ('Thoi-su', 'Thời sự');
+INSERT INTO `app`.`category` (`code`, `name`) VALUES ('The-thao', 'The thao');
+INSERT INTO `app`.`category` (`code`, `name`) VALUES ('Van-hoc', 'Van Hoc');
+INSERT INTO `app`.`category` (`code`, `name`) VALUES ('Thoi-su', 'Thoi su');
 
 INSERT INTO `app`.`new` (`content`, `shortdescription`, `thumbnail`, `title`, `category_id`) VALUES ('The thao 01', 'The thao 01', 'The thao 01', 'The thao 01', '1');
 INSERT INTO `app`.`new` (`content`, `shortdescription`, `thumbnail`, `title`, `category_id`) VALUES ('Van hoc 01', 'Van hoc 01', 'Van hoc 01', 'Van hoc 01', '2');
 INSERT INTO `app`.`new` (`content`, `shortdescription`, `thumbnail`, `title`, `category_id`) VALUES ('Thoi su 01', 'Thoi su 01', 'Thoi su 01', 'Thoi su 01', '3');
 INSERT INTO `app`.`new` (`content`, `shortdescription`, `thumbnail`, `title`, `category_id`) VALUES ('The thao 02', 'The thao 02', 'The thao 02', 'The thao 02', '1');
+
+insert into project(code,name) values('MSW','Project MSW');
+insert into project(code,name) values('DYN','Project DYN');
+insert into project(code,name) values('CSS','Project CSS');
+
+insert into department(code,name) values('IT','Department IT');
+insert into department(code,name) values('ADM','Department ADM');
+insert into department(code,name) values('HR','Department HR');
+
+insert into userdetail(originid,fullname,email,phone,departmentid,manager01id,manager02id, daysalaryamount,daybonusamount,effectivedate,expirationdate) 
+values('2','Employee1','Employee1@gmail.com','034 111000111','1','1','1','300000','400000','2020-10-10','2021-10-10');
+
